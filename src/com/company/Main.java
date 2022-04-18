78ce06c903d2574a65e90348c956ea89ed750fd0
Vipasp <vipasp@yandex.ru>
2022-04-18 10:05:10 +0300

    лаб раб 2_Конопелько О.Н. исправление

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,33 @@
-//Программа расчета суммы трех чисел
+//Угадай
 package com.company;
+import java.util.Random;
 import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
-                int sum = scan();
-                System.out.println("Сумма: " + sum);
+                for(int i = 0; i < 3; i++) {
+
+                        int compNumber = random();
+                        int humNumber = scan();
+                        if (humNumber == compNumber) {
+                                System.out.println("Вы выиграли");
+                        } else if (humNumber < compNumber) {
+                                System.out.println("Вы проиграли");
+                        } else {
+                                System.out.println("Ничья");
                         }
-
-
-
-        public static int scan(){
-                int sum=0;
-                for(int i = 1; i < 4; i++){
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Введите число " + i);
-                int num= scanner.nextInt();
-                sum=sum+num;
                 }
-                return sum;
-
         }
 
+        public static int random() {
+                Random newRandom = new Random();
+                int randoNumber = newRandom.nextInt(10) + 1;
+                return randoNumber;
+        }
 
+        public static int scan(){
+                System.out.println("Введите число от 1 до 10");
+                Scanner scanner = new Scanner(System.in);
+                int humanNumber = scanner.nextInt();
+                return humanNumber;
+        }
 }
