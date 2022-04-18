fe107ba6f0c95cc8d5854080b4bfb0f8133311d5
Vipasp <vipasp@yandex.ru>
2022-04-18 12:23:57 +0300

    лаб раб 2_Конопелько О.Н.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,32 @@
-//Угадай
+///Угадай число
 package com.company;
-import java.util.Random;
 import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
-                for(int i = 0; i < 3; i++) {
+                Scanner scan = new Scanner(System.in);
+                String playAgain = "";
+                do {
 
-                        int compNumber = random();
-                        int humNumber = scan();
-                        if (humNumber == compNumber) {
-                                System.out.println("Вы выиграли");
-                        } else if (humNumber < compNumber) {
-                                System.out.println("Вы проиграли");
-                        } else {
-                                System.out.println("Ничья");
-                        }
-                }
-        }
-
-        public static int random() {
-                Random newRandom = new Random();
-                int randoNumber = newRandom.nextInt(10) + 1;
-                return randoNumber;
-        }
-
-        public static int scan(){
-                System.out.println("Введите число от 1 до 10");
-                Scanner scanner = new Scanner(System.in);
-                int humanNumber = scanner.nextInt();
-                return humanNumber;
+                        int theNumber = (int)(Math.random() * 10 + 1);
+                        int numberOfTries = 0;
+                        int guess = 0;
+                        while (guess != theNumber) { //начинаем цикл
+                                System.out.println("Загадайте число между 1 и 10:");
+                                guess = scan.nextInt();
+                                numberOfTries = numberOfTries + 1;
+                                if (guess < theNumber)
+                                        System.out.println(guess + " это меньше числа, попробуйте еще раз.");
+                                else if (guess > theNumber)
+                                        System.out.println(guess + " это больше числа, попробуйте еще раз.");
+                                else {
+                                        System.out.println(guess + " Правильно! Вы выиграли");
+                                        System.out.println("Вы выиграли за " + numberOfTries + " попытки. Отлично!");
+                                }
+                        } // Конец цикла
+                        System.out.println("Сыграем еще? (y/n)?");
+                        playAgain = scan.next();
+                } while (playAgain.equalsIgnoreCase("y"));
+                System.out.println("Спасибо за игру. Пока!");
+                scan.close();
         }
 }
